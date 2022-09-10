6e733038a913a24f975c388e9818bf1b0f20e262
Daniel <daniel@ge1ger.de>
2022-09-10 17:52:55 +0200

    remove pull to refresh library

--- lib/view/shopping_list/refreshable_scroll_view.dart
+++ lib/view/shopping_list/refreshable_scroll_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class RefreshableScrollView extends StatefulWidget {
+class RefreshableScrollView extends StatelessWidget {
   final Widget child;
   final Future<void> Function()? onRefresh;
 
@@ -9,38 +8,28 @@ class RefreshableScrollView extends StatefulWidget {
       : super(key: key);
 
   @override
-  State<RefreshableScrollView> createState() => _RefreshableScrollViewState();
-}
-
-class _RefreshableScrollViewState extends State<RefreshableScrollView> {
-  late final RefreshController _refreshController;
-
-  @override
-  void initState() {
-    _refreshController = RefreshController(initialRefresh: true);
-    super.initState();
-  }
-
-  void _onRefresh() async {
-    try {
-      await widget.onRefresh!();
-      _refreshController.refreshCompleted();
-    } catch (e) {
-      _refreshController.refreshFailed();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    Widget scrollViewContainingChild = SingleChildScrollView(
-        scrollDirection: Axis.vertical, child: Center(child: widget.child));
-    if (widget.onRefresh == null) {
+    Widget scrollViewContainingChild = LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+      return SingleChildScrollView(
+          // necessary for RefreshIndicator to show
+          physics: const AlwaysScrollableScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          child: ConstrainedBox(
+            constraints:
+                BoxConstraints(minHeight: viewportConstraints.maxHeight),
+            child: Container(
+              child: child,
+              alignment: Alignment.topCenter,
+            ),
+          ));
+    });
+    if (onRefresh == null) {
       return scrollViewContainingChild;
     }
-    return SmartRefresher(
-        controller: _refreshController,
-        header: const MaterialClassicHeader(),
-        onRefresh: _onRefresh,
+
+    return RefreshIndicator(
+        onRefresh: onRefresh ?? () => Future.value(),
         child: scrollViewContainingChild);
   }
 }
